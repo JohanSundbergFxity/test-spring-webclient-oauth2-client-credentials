786fe55b666cb0b08ee31954a6c7031c9860b391
Johan Sundberg <johan.sundberg@fxity.net>
2021-04-13 08:01:01 +0200

    init

---
+++ src/main/java/com/example/springwebclientoauth2clientcredentials/SpringWebclientOauth2ClientCredentialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebclientoauth2clientcredentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebclientOauth2ClientCredentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebclientOauth2ClientCredentialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springwebclientoauth2clientcredentials/SpringWebclientOauth2ClientCredentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebclientoauth2clientcredentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebclientOauth2ClientCredentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
